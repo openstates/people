c982592c32052ad6d3f197748f1e39b954b8d842
James Turk <james.p.turk@gmail.com>
2018-10-11 13:59:30 -0400

    test retirement

--- scripts/retire.py
+++ scripts/retire.py
@@ -5,32 +5,48 @@ import click
 from utils import load_yaml, dump_obj, role_is_active
 
 
-def retire_from_committees(person_id, end_date, committee_dir):
+def retire_from_committee(committee, person_id, end_date):
     num = 0
-
-    for com_filename in glob.glob(os.path.join(committee_dir, '*.yml')):
-        with open(com_filename) as f:
-            committee = load_yaml(f)
-
-        for role in committee['memberships']:
-            if role.get('id') == person_id and role_is_active(role):
-                role['end_date'] = end_date
-                num += 1
-                dump_obj(committee, filename=com_filename)
-    return num
+    for role in committee['memberships']:
+        if role.get('id') == person_id and role_is_active(role):
+            role['end_date'] = end_date
+            num += 1
+    return committee, num
 
 
-def retire_person(filename, end_date):
-    with open(filename) as f:
-        person = load_yaml(f)
-
-    committee_dir = os.path.join(os.path.dirname(filename), '../organizations')
-    num = retire_from_committees(person['id'], end_date, committee_dir)
-
+def retire_person(person, end_date):
+    num = 0
     for role in person['roles']:
         if role_is_active(role):
             role['end_date'] = end_date
             num += 1
+    return person, num
+
+
+def move_file(filename):
+    new_filename = filename.replace('/people/', '/retired/')
+    click.secho(f'moved from {filename} to {new_filename}')
+    os.renames(filename, new_filename)
+
+
+@click.command()
+@click.argument('end_date')
+@click.argument('filename')
+def retire(end_date, filename):
+    # end the person's active roles & re-save
+    with open(filename) as f:
+        person = load_yaml(f)
+    person, num = retire_person(person, end_date)
+    dump_obj(person, filename=filename)
+
+    # same for their committees
+    committee_glob = os.path.join(os.path.dirname(filename), '../organizations/*.yml')
+    for com_filename in glob.glob(committee_glob):
+        with open(com_filename) as f:
+            committee = load_yaml(f)
+        committee, num_roles = retire_from_committee(committee, person['id'], end_date)
+        dump_obj(committee, filename=com_filename)
+        num += num_roles
 
     if num == 0:
         click.secho('no active roles to retire', fg='red')
@@ -39,18 +55,6 @@ def retire_person(filename, end_date):
     else:
         click.secho(f'retired person from {num} roles')
 
-    dump_obj(person, filename=filename)
-
-
-def move_file(filename):
-    os.renames(filename, filename.replace('/people/', '/retired/'))
-
-
-@click.command()
-@click.argument('end_date')
-@click.argument('filename')
-def retire(end_date, filename):
-    retire_person(filename, end_date)
     move_file(filename)
 
 
