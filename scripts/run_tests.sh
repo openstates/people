0890a7a58249bb1b15a53032c7c132996883a793
James Turk <james.p.turk@gmail.com>
2018-10-12 23:57:50 -0400

    coveralls

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 PYTHONPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-pytest --cov scripts --ds=tests.django_test_settings
+pytest --cov scripts --cov-report html --ds=tests.django_test_settings
