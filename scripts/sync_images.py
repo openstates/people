5f3e392ca037f5db46bea247c63c08ab2d08f030
James Turk <james.p.turk@gmail.com>
2018-11-14 21:37:30 -0500

    --skip-existing and mimetype logic

--- scripts/sync_images.py
+++ scripts/sync_images.py
@@ -8,29 +8,44 @@ from botocore.exceptions import ClientError
 import requests
 from utils import get_all_abbreviations, iter_objects
 
+ALLOWED_CONTENT_TYPES = ('image/jpeg', 'image/png', 'image/gif', 'image/jpg')
 
-def download_state_images(abbr):
+
+def download_state_images(abbr, skip_existing):
     s3 = boto3.client('s3')
     for person, _ in iter_objects(abbr, 'people'):
         url = person.get('image')
+        if not url:
+            continue
         person_id = person.get('id')
+        key_name = 'images/original/' + person_id
+
+        try:
+            obj = s3.head_object(Bucket=os.environ['S3_BUCKET'], Key=key_name)
+        except ClientError:
+            obj = None
+
+        if obj and skip_existing:
+            click.secho(f'{key_name} already exists', fg='green')
+            continue
+
+        # get the source URL
         resp = requests.get(url)
 
         if resp.status_code != 200:
             click.secho(f'could not fetch {url}, {resp.status_code}', fg='red')
             continue
 
+        content_type = resp.headers['content-type']
+        if content_type not in ALLOWED_CONTENT_TYPES:
+            click.secho(f'unknown content type for {url}, {content_type}', fg='red')
+            continue
+
+        # compare sha1 hashes
         sha1 = hashlib.sha1(resp.content).hexdigest()
-        key_name = 'images/original/' + person_id
-
-        try:
-            obj = s3.head_object(Bucket=os.environ['S3_BUCKET'], Key=key_name)
-
-            if obj['Metadata']['sha1'] == sha1:
-                click.secho(f'{key_name} already up to date', fg='yellow')
-                continue
-        except ClientError:
-            pass
+        if obj and obj['Metadata']['sha1'] == sha1:
+            click.secho(f'{key_name} already up to date', fg='yellow')
+            continue
 
         s3.upload_fileobj(
             io.BytesIO(resp.content),
@@ -38,7 +53,7 @@ def download_state_images(abbr):
             key_name,
             ExtraArgs={
                 'Metadata': {'sha1': sha1},
-                'ContentType': resp.headers['content-type'],
+                'ContentType': content_type,
             }
         )
         click.secho(f'copied {url} to {key_name}', fg='green')
@@ -46,7 +61,8 @@ def download_state_images(abbr):
 
 @click.command()
 @click.argument('abbreviations', nargs=-1)
-def sync_images(abbreviations):
+@click.option('--skip-existing/--no-skip-existing')
+def sync_images(abbreviations, skip_existing):
     """
         Download images and sync them to S3.
 
@@ -56,7 +72,7 @@ def sync_images(abbreviations):
         abbreviations = get_all_abbreviations()
 
     for abbr in abbreviations:
-        download_state_images(abbr)
+        download_state_images(abbr, skip_existing)
 
 
 if __name__ == '__main__':
