2cf99fe0cfda305523c89decddf1ef2311b9d685
James Turk <james.p.turk@gmail.com>
2018-11-15 00:13:13 -0500

    resize WIP

--- scripts/sync_images.py
+++ scripts/sync_images.py
@@ -4,64 +4,107 @@ import io
 import hashlib
 import click
 import boto3
+from PIL import Image
 from botocore.exceptions import ClientError
 import requests
 from utils import get_all_abbreviations, iter_objects
 
+
 ALLOWED_CONTENT_TYPES = ('image/jpeg', 'image/png', 'image/gif', 'image/jpg')
+s3 = boto3.client('s3')
+
+
+def upload(img_callable, key_name, skip_existing):
+    try:
+        obj = s3.head_object(Bucket=os.environ['S3_BUCKET'], Key=key_name)
+    except ClientError:
+        obj = None
+
+    if obj and skip_existing:
+        click.secho(f'{key_name} already exists', fg='yellow')
+        return
+
+    # if we need to get the object, call the (potentially expensive) callable
+    img_bytes, content_type = img_callable()
+    if not img_bytes:
+        return
+
+    # compare sha1 hashes
+    sha1 = hashlib.sha1(img_bytes).hexdigest()
+    if obj and obj['Metadata'].get('sha1') == sha1:
+        click.secho(f'{key_name} already up to date', fg='yellow')
+        return img_bytes
+
+    click.secho(f'uploading {key_name}', fg='green')
+    s3.upload_fileobj(
+        io.BytesIO(img_bytes),
+        os.environ['S3_BUCKET'],
+        key_name,
+        ExtraArgs={
+            'Metadata': {'sha1': sha1},
+            'ContentType': content_type,
+            'ACL': 'public-read',
+        }
+    )
+
+    return img_bytes
+
+
+def download_image(url):
+    # get the source URL
+    try:
+        resp = requests.get(url)
+    except Exception as e:
+        click.secho(f'could not fetch {url}, {e}', fg='red')
+        return None, None
+
+    if resp.status_code != 200:
+        click.secho(f'could not fetch {url}, {resp.status_code}', fg='red')
+        return None, None
+
+    content_type = resp.headers['content-type']
+    if content_type not in ALLOWED_CONTENT_TYPES:
+        click.secho(f'unknown content type for {url}, {content_type}', fg='red')
+        return None, None
+
+    return resp.content, content_type
+
+
+def resize_image(img_bytes, size):
+    img = Image.open(fp=io.BytesIO(img_bytes))
+    img = img.convert('RGB')
+    img.thumbnail((size, size))
+    output = io.BytesIO()
+    img.save(output, "JPEG", quality=80, progressive=True)
+    output.seek(0)
+    return output.read(), 'image/jpeg'
 
 
 def download_state_images(abbr, skip_existing):
-    s3 = boto3.client('s3')
     for person, _ in iter_objects(abbr, 'people'):
         url = person.get('image')
+        person_id = person['id']
         if not url:
             continue
-        person_id = person.get('id')
-        key_name = 'images/original/' + person_id
 
-        try:
-            obj = s3.head_object(Bucket=os.environ['S3_BUCKET'], Key=key_name)
-        except ClientError:
-            obj = None
-
-        if obj and skip_existing:
-            click.secho(f'{key_name} already exists', fg='green')
+        img_bytes = upload(lambda: download_image(url),
+                           f'images/original/{person_id}',
+                           skip_existing)
+        if not img_bytes:
             continue
 
-        # get the source URL
-        try:
-            resp = requests.get(url)
-        except Exception as e:
-            click.secho(f'could not fetch {url}, {e}', fg='red')
-            continue
+        upload(lambda: resize_image(img_bytes, 200),
+               f'images/small/{person_id}',
+               skip_existing)
 
-        if resp.status_code != 200:
-            click.secho(f'could not fetch {url}, {resp.status_code}', fg='red')
-            continue
 
-        content_type = resp.headers['content-type']
-        if content_type not in ALLOWED_CONTENT_TYPES:
-            click.secho(f'unknown content type for {url}, {content_type}', fg='red')
-            continue
-
-        # compare sha1 hashes
-        sha1 = hashlib.sha1(resp.content).hexdigest()
-        if obj and obj['Metadata']['sha1'] == sha1:
-            click.secho(f'{key_name} already up to date', fg='yellow')
-            continue
-
-        s3.upload_fileobj(
-            io.BytesIO(resp.content),
-            os.environ['S3_BUCKET'],
-            key_name,
-            ExtraArgs={
-                'Metadata': {'sha1': sha1},
-                'ContentType': content_type,
-            }
-        )
-        click.secho(f'copied {url} to {key_name}', fg='green')
-
+# def recognize(key):
+#     client = boto3.client('rekognition')
+#     resp = client.detect_faces(Image={'S3Object': {'Bucket': os.environ['S3_BUCKET'],
+    #                                                'Name': key}},
+    #                            Attributes=["DEFAULT"])
+    # algorithm suggested here seems like a good starting point
+    # https://stackoverflow.com/questions/4813608/cropping-an-image-with-a-focus-area-face-using-imagemagick
 
 @click.command()
 @click.argument('abbreviations', nargs=-1)
