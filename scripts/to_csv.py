7e5722eef8890049b4fd80c610c0ffde7698d551
James Turk <dev@jamesturk.net>
2019-04-09 19:49:50 -0400

    to_csv fixes, addition of contact details

--- scripts/to_csv.py
+++ scripts/to_csv.py
@@ -3,7 +3,7 @@ import os
 import csv
 import glob
 import click
-from utils import (get_data_dir, get_jurisdiction_id, get_all_abbreviations, get_districts,
+from utils import (get_data_dir, get_jurisdiction_id, get_all_abbreviations,
                    get_settings, load_yaml, role_is_active)
 
 
@@ -27,6 +27,8 @@ def write_csv(files, jurisdiction_id, output_filename):
              "current_party", "current_district", "current_chamber",
              "given_name", "family_name", "gender", "biography", "birth_date", "death_date",
              "image", "links", "sources",
+             "capitol_address", "capitol_email", "capitol_voice", "capitol_fax",
+             "district_address", "district_email", "district_voice", "district_fax",
              "twitter", "youtube", "instagram", "facebook",
              )
         )
@@ -48,8 +50,25 @@ def write_csv(files, jurisdiction_id, output_filename):
                         current_chamber = role["type"]
                         current_district = role["district"]
 
-                links = ";".join(l["url"] for l in data["links"])
-                sources = ";".join(l["url"] for l in data["links"])
+                district_address = district_email = district_voice = district_fax = None
+                capitol_address = capitol_email = capitol_voice = capitol_fax = None
+                for cd in data.get("contact_details", {}):
+                    note = cd["note"].lower()
+                    if "district" in note:
+                        district_address = cd.get("address")
+                        district_email = cd.get("email")
+                        district_voice = cd.get("voice")
+                        district_fax = cd.get("fax")
+                    elif "capitol" in note:
+                        capitol_address = cd.get("address")
+                        capitol_email = cd.get("email")
+                        capitol_voice = cd.get("voice")
+                        capitol_fax = cd.get("fax")
+                    else:
+                        click.secho("unknown office: " + note, fg="red")
+
+                links = ";".join(l["url"] for l in data.get("links", []))
+                sources = ";".join(l["url"] for l in data.get("sources", []))
 
                 obj = {
                     "id": data["id"],
@@ -63,17 +82,23 @@ def write_csv(files, jurisdiction_id, output_filename):
                     "biography": data.get("biography"),
                     "birth_date": data.get("birth_date"),
                     "death_date": data.get("death_date"),
-                    "image": data["image"],
+                    "image": data.get("image"),
                     "twitter": data.get("ids", {}).get("twitter"),
                     "youtube": data.get("ids", {}).get("youtube"),
                     "instagram": data.get("ids", {}).get("instagram"),
                     "facebook": data.get("ids", {}).get("facebook"),
                     "links": links,
                     "sources": sources,
-                    # TODO: contact details
+                    "district_address": district_address,
+                    "district_email": district_email,
+                    "district_voice": district_voice,
+                    "district_fax": district_fax,
+                    "capitol_address": capitol_address,
+                    "capitol_email": capitol_email,
+                    "capitol_voice": capitol_voice,
+                    "capitol_fax": capitol_fax,
                 }
                 out.writerow(obj)
-
 
     click.secho(f'processed {len(files)} files', fg='green')
 
@@ -93,11 +118,8 @@ def to_csv(abbreviations):
         click.secho('==== {} ===='.format(abbr), bold=True)
         directory = get_data_dir(abbr)
         jurisdiction_id = get_jurisdiction_id(abbr)
-
         person_files = glob.glob(os.path.join(directory, 'people/*.yml'))
-        state_settings = settings[abbr]
-
-        write_csv(person_files, jurisdiction_id, "out.csv")
+        write_csv(person_files, jurisdiction_id, f"csv/{abbr}_legislators.csv")
 
 
 if __name__ == '__main__':
