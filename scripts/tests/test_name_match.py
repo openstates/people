bf42fc922e3ac1ac47fdad285277866cd8ef8e71
schneidy <schneiderman.daniel@gmail.com>
2020-02-12 17:10:43 -0500

    Added more test cases for name matchign

--- scripts/tests/test_name_match.py
+++ scripts/tests/test_name_match.py
@@ -7,7 +7,22 @@ from name_match import find_match
         # Basic Name
         ("Dan Schneiderman", {"name": "Dan Schneiderman", "family_name": "Schneiderman"}, True),
 
+        # Just Last Name
+        ("Schneiderman", {"name": "Dan Schneiderman", "family_name": "Schneiderman"}, True),
+
+        # Name with a comma
+        ("Kwan, Karen", {"name": "Karen Kwan", "family_name": "Kwan"}, True),
+
+        # Name with an initial at the end Matt Huffman, M.
+        ("Matt Huffman, M", {"name": "Matt Huffman", "family_name": "Huffman"}, True),
+
+        # Last name of location ZEIGLER of Montville
+        ("ZEIGLER of Montville", {"name": "Stanley Paige Zeigler", "family_name": "Zeigler"}, True),
+
+        # Full name with initial and number at end of name
+        ("Louis W. Blessing, III", {"name": "Louis W. Blessing", "family_name": "Blessing"}, True),
+
     ],
 )
-def test_family_name_simple(csv_name, yaml_person, expected):
+def test_family_name(csv_name, yaml_person, expected):
     assert find_match(csv_name, yaml_person) == expected
