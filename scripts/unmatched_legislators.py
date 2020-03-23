d1a5afc7cc06f24f936c467be397ea0fe9eb54ee
James Turk <dev@jamesturk.net>
2020-03-23 12:36:26 -0400

    speed up using aggregate queries

--- scripts/unmatched_legislators.py
+++ scripts/unmatched_legislators.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python
 """Script to create CSVs of unmatched legislators given a state and session"""
-
 import csv
 from collections import Counter, defaultdict
 from utils import get_jurisdiction_id, init_django
 import click
 
 
-init_django()
-
-from opencivicdata.legislative.models import PersonVote, BillSponsorship  # noqa
-
-
 def archive_leg_to_csv(state_abbr=None):
+    from opencivicdata.legislative.models import PersonVote, BillSponsorship  # noqa
+    from django.db.models import Count, F
+
     output_filename = f"unmatched_{state_abbr}.csv"
 
     jurisdiction_id = get_jurisdiction_id(state_abbr)
@@ -22,21 +19,29 @@ def archive_leg_to_csv(state_abbr=None):
     missing_sponsors = Counter()
     sessions_for_name = defaultdict(set)
 
-    voters = PersonVote.objects.filter(
-        vote_event__legislative_session__jurisdiction_id=jurisdiction_id, voter_id=None
-    ).select_related("vote_event__legislative_session")
+    voters = (
+        PersonVote.objects.filter(
+            vote_event__legislative_session__jurisdiction_id=jurisdiction_id, voter_id=None,
+        )
+        .values("voter_name", session=F("vote_event__legislative_session__identifier"))
+        .annotate(n=Count("id"))
+    )
     for voter in voters:
-        missing_votes[voter.voter_name] += 1
-        sessions_for_name[voter.voter_name].add(voter.vote_event.legislative_session.identifier)
+        missing_votes[voter["voter_name"]] += voter["n"]
+        sessions_for_name[voter["voter_name"]].add(voter["session"])
 
-    bill_sponsors = BillSponsorship.objects.filter(
-        bill__legislative_session__jurisdiction_id=jurisdiction_id,
-        person_id=None,
-        organization_id=None,
-    ).select_related("bill__legislative_session")
+    bill_sponsors = (
+        BillSponsorship.objects.filter(
+            bill__legislative_session__jurisdiction_id=jurisdiction_id,
+            person_id=None,
+            organization_id=None,
+        )
+        .values("name", session=F("bill__legislative_session__identifier"))
+        .annotate(n=Count("id"))
+    )
     for bill_sponsor in bill_sponsors:
-        missing_sponsors[bill_sponsor.name] += 1
-        sessions_for_name[bill_sponsor.name].add(bill_sponsor.bill.legislative_session.identifier)
+        missing_sponsors[bill_sponsor["name"]] += 1
+        sessions_for_name[bill_sponsor["name"]].add(bill_sponsor["session"])
 
     all_names = sorted(sessions_for_name.keys())
 
@@ -61,4 +66,5 @@ def export_unmatched(state_abbr=None, session=None):
 
 
 if __name__ == "__main__":
+    init_django()
     export_unmatched()
