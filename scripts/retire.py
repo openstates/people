d795d55f05a7effa75b686ff5484dc260260af88
James Turk <dev@jamesturk.net>
2020-07-24 11:16:41 -0400

    update retire for municipalities

--- scripts/retire.py
+++ scripts/retire.py
@@ -32,6 +32,7 @@ def retire_person(person, end_date, reason=None, death=False):
 
 def move_file(filename):  # pragma: no cover
     new_filename = filename.replace("/legislature/", "/retired/")
+    new_filename = filename.replace("/municipalities/", "/retired/")
     click.secho(f"moved from {filename} to {new_filename}")
     os.renames(filename, new_filename)
 
