94afd2a4ae096923c19904789d719de56f4970a0
James Turk <dev@jamesturk.net>
2020-08-21 15:39:09 -0400

    PGPORT

--- scripts/tests/django_test_settings.py
+++ scripts/tests/django_test_settings.py
@@ -13,6 +13,7 @@ DATABASES = {
         "USER": os.environ.get("PGUSER", "openstates"),
         "PASSWORD": os.environ.get("PGPASS", "openstates"),
         "HOST": os.environ.get("PGHOST", "localhost"),
+        "PORT": os.environ.get("PGPORT", 5432),
     }
 }
 MIDDLEWARE_CLASSES = ()
