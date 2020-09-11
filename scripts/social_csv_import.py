f7fb88945e2789ceccb6f2adaf48e16a48d3b99c
James Turk <dev@jamesturk.net>
2020-09-11 12:17:53 -0400

    actually update files w/ ids

--- scripts/social_csv_import.py
+++ scripts/social_csv_import.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python
 import os
-import sys
 import csv
 import glob
 import click
 from utils import (
     get_data_dir,
-    get_jurisdiction_id,
-    get_all_abbreviations,
     load_yaml,
-    role_is_active,
+    dump_obj,
 )
 
 
@@ -21,14 +18,28 @@ def load_person_by_id(abbr, person_id):
     person = glob.glob(os.path.join(directory, "*", f"*{person_id}.yml"))
     if len(person) < 1:
         click.secho(f"could not find {abbr} {person_id}")
-        return
+        return None, None
     elif len(person) > 1:
         click.secho(f"multiple matches for {abbr} {person_id}")
-        return
+        return None, None
 
     # found them, load & return
     with open(person[0]) as f:
-        return load_yaml(f)
+        return person[0], load_yaml(f)
+
+
+def add_id_if_exists(person, id_type, id_or_none):
+    if id_or_none:
+        existing = person.get("ids", {}).get(id_type)
+        # doesn't yet exist, set it
+        if not existing:
+            if "ids" not in person:
+                person["ids"] = {}
+            person["ids"][id_type] = id_or_none
+            click.secho(f"set {person['id']} {id_type} to {id_or_none}")
+        # already exists, conflict
+        if existing and existing != id_or_none:
+            click.secho(f"conflict for {person['id']} {id_type} old={existing}, new={id_or_none}")
 
 
 @click.command()
@@ -40,16 +51,14 @@ def social_csv_import(abbr, filename):
 
         for line in social_data:
             person_id = line["id"]
-            person = load_person_by_id(abbr, person_id)
+            person_fname, person = load_person_by_id(abbr, person_id)
             if not person:
                 return
-            print(person["name"])
 
-            twitter = line.get("twitter")
-            facebook = line.get("facebook")
-            youtube = line.get("youtube")
-            instagram = line.get("instagram")
-            linkedin = line.get("linkedin")
+            for id_type in ("twitter", "facebook", "youtube", "instagram", "linkedin"):
+                add_id_if_exists(person, id_type, line.get(id_type))
+
+            dump_obj(person, filename=person_fname)
 
 
 if __name__ == "__main__":
