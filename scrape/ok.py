255a5ede30d59be5f8f08ef6c5d79a25fdd3b578
James Turk <dev@jamesturk.net>
2020-12-21 20:17:28 -0500

    OK: add house address

--- scrape/ok.py
+++ scrape/ok.py
@@ -3,6 +3,7 @@ from spatula.utils import HtmlListPage, SimilarLink, HtmlPage, CSS
 
 class OKSenateList(HtmlListPage):
     selector = SimilarLink("https://oksenate.gov/senators/", num_items=48)
+    url = "https://oksenate.gov/senators"
 
     def __init__(self):
         super().__init__(url="https://oksenate.gov/senators")
@@ -46,4 +47,21 @@ class OKHouseDetail(HtmlPage):
         data["name"] = self.name_css.match_one(self.root).text.split(maxsplit=1)[1]
         data["district"] = self.district_css.match_one(self.root).text
         data["party"] = self.party_css.match_one(self.root).text
+
+        contact_url = self.url.replace("District.aspx", "Contact.aspx")
+        assert contact_url.startswith("https://www.okhouse.gov/Members/Contact.aspx?District=")
+        data["contact_url"] = contact_url
+
+        # capitol address
+        check_capitol_address = CSS(".districtheadleft").match(self.root)[0].text_content().strip()
+        if check_capitol_address == "Capitol Address:":
+            capitol_address_div = (
+                CSS(".districtheadleft + div")
+                .match(self.root)[0]
+                .text_content()
+                .strip()
+                .splitlines()
+            )
+            data["address"] = "; ".join([ln.strip() for ln in capitol_address_div[:-1]])
+            data["phone"] = capitol_address_div[-1].strip()
         return data
