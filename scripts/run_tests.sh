32abeebdf811a3fae743b7069ca0c0b12f11d85c
James Turk <dev@jamesturk.net>
2020-12-29 20:59:09 -0500

    fix run_tests.sh

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-PYTHONPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-poetry run pytest scripts --cov scripts --cov-report html --ds=tests.django_test_settings
+PYTHONPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )" poetry run pytest scripts --cov scripts --cov-report html --ds=tests.django_test_settings
