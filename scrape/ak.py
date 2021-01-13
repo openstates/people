4bcf5394c41a9934a11289fa4bec78de2b615f0e
James Turk <dev@jamesturk.net>
2021-01-13 17:55:03 -0500

    convert AK to spatula

--- scrape/ak.py
+++ scrape/ak.py
@@ -1,11 +1,7 @@
-#!/usr/bin/env python
-"""
-Prototype new-style scraper for direct-to-YAML scraping of Alaska
-"""
-import scrapelib
-import lxml.etree
 import re
-import os
+from spatula.pages import XmlListPage
+from spatula.sources import URL
+from spatula.selectors import XPath
 from common import Person
 
 
@@ -34,18 +30,16 @@ def _get_if_exists(item, elem):
         return str(val[0])
 
 
-class AlaskaScraper(scrapelib.Scraper):
-    def scrape_legislator_list(self, session_num):
-        url = (
-            "http://www.legis.state.ak.us/publicservice/basis/members"
-            "?minifyresult=false&session=" + session_num
-        )
-        xml = scrapelib.Scraper().get(url).content
-        for line in lxml.etree.fromstring(xml).xpath("//Member/MemberDetails"):
-            person = self.handle_list_item(line, session_num)
-            yield person
+class Legislators(XmlListPage):
+    session_num = "32"
+    source = URL(
+        "http://www.legis.state.ak.us/publicservice/basis/members"
+        f"?minifyresult=false&session={session_num}",
+        headers={"X-Alaska-Legislature-Basis-Version": "1.4"},
+    )
+    selector = XPath("//Member/MemberDetails")
 
-    def handle_list_item(self, item, session_num):
+    def process_item(self, item):
         item_dict = {elem: _get_if_exists(item, elem) for elem in ELEMENTS}
         chamber = item.attrib["chamber"]
         code = item.attrib["code"].lower()
@@ -59,15 +53,16 @@ class AlaskaScraper(scrapelib.Scraper):
             chamber=("upper" if chamber == "S" else "lower"),
             district=item_dict["District"],
             image=f"http://akleg.gov/images/legislators/{code}.jpg",
+            email=item_dict["EMail"],
         )
         person.add_link(
-            "http://www.akleg.gov/basis/Member/Detail/{}?code={}".format(session_num, code)
+            "http://www.akleg.gov/basis/Member/Detail/{}?code={}".format(self.session_num, code)
         )
         person.add_source("http://w3.akleg.gov/")
 
-        phone = "907-" + item_dict["Phone"][0:3] + "-" + item_dict["Phone"][3:]
-        person.capitol_office.voice = phone
-        person.capitol_office.email = item_dict["EMail"]
+        if item_dict["Phone"]:
+            phone = "907-" + item_dict["Phone"][0:3] + "-" + item_dict["Phone"][3:]
+            person.capitol_office.voice = phone
 
         if item_dict["Building"] == "CAPITOL":
             person.capitol_office.address = "State Capitol Room {}; Juneau AK, 99801".format(
@@ -75,17 +70,3 @@ class AlaskaScraper(scrapelib.Scraper):
             )
 
         return person
-
-
-def main():
-    try:
-        os.makedirs("incoming/ak/people")
-    except OSError:
-        pass
-    ak = AlaskaScraper()
-    for leg in ak.scrape_legislator_list("31"):
-        leg.save("incoming/ak/people")
-
-
-if __name__ == "__main__":
-    main()
