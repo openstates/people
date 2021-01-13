b6ae8e389311702781384493ccb7974a8a006e26
James Turk <dev@jamesturk.net>
2021-01-13 17:55:18 -0500

    add headers to URL source

--- scrape/spatula/sources.py
+++ scrape/spatula/sources.py
@@ -3,13 +3,16 @@ class Source:
 
 
 class URL(Source):
-    def __init__(self, url, method="GET", data=None):
+    def __init__(self, url, method="GET", data=None, headers=None):
         self.url = url
         self.method = method
         self.data = data
+        self.headers = headers
 
     def get_data(self, scraper):
-        return scraper.request(method=self.method, url=self.url, data=self.data).content
+        return scraper.request(
+            method=self.method, url=self.url, data=self.data, headers=self.headers
+        ).content
 
     def __str__(self):
         return self.url
