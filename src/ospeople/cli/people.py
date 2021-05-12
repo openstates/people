c1d225c8eb7b394d2f85ebf7294fe4be6ff19464
James Turk <dev@jamesturk.net>
2021-05-12 17:52:20 -0400

    summarize into new people tool

--- src/ospeople/cli/people.py
+++ src/ospeople/cli/people.py
@@ -1,8 +1,115 @@
 import os
+import glob
 import click
+from collections import Counter, defaultdict
 from openstates.utils import abbr_to_jid
 from ..models.people import Person, Role, Party, Link
-from ..utils import ocd_uuid, get_data_dir, dump_obj
+from ..utils import ocd_uuid, get_data_dir, dump_obj, get_all_abbreviations, load_yaml
+
+
+OPTIONAL_FIELD_SET = {
+    "sort_name",
+    "given_name",
+    "family_name",
+    "gender",
+    "summary",
+    "biography",
+    "birth_date",
+    "image",
+    "email",
+    "other_names",
+}
+
+
+class Summarizer:
+    def __init__(self) -> None:
+        self.person_count = 0
+        self.optional_fields: Counter[str] = Counter()
+        self.extra_counts: Counter[str] = Counter()
+        self.contact_counts: Counter[str] = Counter()
+        self.id_counts: Counter[str] = Counter()
+        self.parties: Counter[str] = Counter()
+        self.active_legislators: defaultdict[str, defaultdict[str, list[Person]]] = defaultdict(
+            lambda: defaultdict(list)
+        )
+
+    def summarize(self, person: Person) -> None:
+        self.person_count += 1
+        self.optional_fields.update(
+            set(person.dict(exclude_defaults=True).keys()) & OPTIONAL_FIELD_SET
+        )
+        self.extra_counts.update(person.extras.keys())
+
+        district = role_type = None
+        for role in person.roles:
+            if role.is_active():
+                role_type = role.type
+                district = role.district
+                break
+        if role_type:
+            self.active_legislators[role_type][district].append(person)
+
+        for role in person.party:
+            if role.is_active():
+                self.parties[role.name] += 1
+
+        for cd in person.contact_details:
+            for key in ("voice", "fax", "address"):
+                if getattr(cd, key, None):
+                    self.contact_counts[cd.note + " " + key] += 1
+
+        for id_ in person.ids or []:
+            self.id_counts[id_.scheme] += 1
+        for id_ in person.other_identifiers:
+            if id_.scheme not in ("openstates", "legacy_openstates"):
+                self.id_counts[id._scheme] += 1
+
+    def print_summary(self) -> None:  # pragma: no cover
+        click.secho(
+            f"processed {self.person_count} active people",
+            bold=True,
+        )
+        for role_type in self.active_legislators:
+            count = sum([len(v) for v in self.active_legislators[role_type].values()])
+            click.secho(f"{count:4d} {role_type}")
+
+        click.secho("Parties", bold=True)
+        for party, count in self.parties.items():
+            if party == "Republican":
+                color = "red"
+            elif party == "Democratic":
+                color = "blue"
+            else:
+                color = "green"
+            click.secho(f"{count:4d} {party} ", bg=color)
+
+        for name, collection in {
+            "Contact Info": self.contact_counts,
+            "Identifiers": self.id_counts,
+            "Additional Info": self.optional_fields,
+            "Extras": self.extra_counts,
+        }.items():
+            if collection:
+                click.secho(name, bold=True)
+                for type, count in collection.items():
+                    click.secho(f" {type:<25} {count:4d} {count/self.person_count*100:.0f}% ")
+            else:
+                click.secho(name + " - none", bold=True)
+
+    def print_roster(self) -> None:  # pragma: no cover
+        for role_type, districts in self.active_legislators.items():
+            for district, people in sorted(districts.items()):
+                click.secho(f"{role_type} {district}", fg="blue")
+                for person in people:
+                    click.secho(f"   {person.name}")
+
+    def process_legislature(self, abbr: str) -> None:  # pragma: no cover
+        filenames = glob.glob(os.path.join(get_data_dir(abbr), "legislature", "*.yml"))
+
+        for filename in filenames:
+            with open(filename) as f:
+                person = Person(**load_yaml(f))
+                self.summarize(person)
 
 
 def create_person(
@@ -96,6 +203,26 @@ def new(
         email=email,
         start_date=start_date,
     )
+
+
+@main.command()
+@click.argument("abbreviations", nargs=-1)
+@click.option("--roster/--no-roster", default=False, help="Print roster after summary.")
+def summarize(abbreviations: list[str], roster: bool) -> None:
+    """
+    Provide summary of a jurisdiction's data.
+
+    <ABBR> can be provided to restrict linting to single state's files.
+    """
+    if not abbreviations:
+        abbreviations = get_all_abbreviations()
+
+    summarizer = Summarizer()
+    for abbr in abbreviations:
+        summarizer.process_legislature(abbr)
+    summarizer.print_summary()
+    if roster:
+        summarizer.print_roster()
 
 
 if __name__ == "__main__":
