333ba00dfae97a2d734dd45088ec44db1d6224f4
Rypo <ryan@gust.dev>
2021-05-20 19:03:37 -0500

    MO: add csv extraction logic and convert to CsvListPage

--- src/ospeople/scrape/mo.py
+++ src/ospeople/scrape/mo.py
@@ -1,5 +1,8 @@
+import io
+import csv
 import attr
-from spatula import HtmlListPage, HtmlPage, URL, CSS
+import lxml.html
+from spatula import HtmlPage, URL, CSS, CsvListPage
 from .common.people import Person, PeopleWorkflow
 
 
@@ -13,30 +16,57 @@ class HousePartial:
     voice: str
     room: str
     url: str
+    # Additional Fields (not displayed on webpage)
+    # leadership_position: str # LeadershipPositionCurrent # EX: "Assistant Majority Floor Leader"
+    # year_elected: str # YearEnter # datetime YYYY
+    # floor: str # Floor # int 1,2,3,4
+    # gender: str # Gender # str M,F
 
 
-class HouseList(HtmlListPage):
-    # note: there is a CSV, but it requires a bunch of ASP.net hoops to actually get
+def extract_csv(response):
+    """aka ASP.net hoops"""
+    # This is effectively submitting a form to extract the CSV file we're after.
+    # The cost is making 2 requests instead of 1. The benefit is a CSV file with additional fields.
+    # The same could be done (albeit much more verbosely) by using a spatula.URL POST.
+    # We would need to scrape the `value` attr for each #id=["__VIEWSTATE", "__EVENTVALIDATION", "btnCSV"]
+    # anyway, so making 2 requests is unavoidable, assuming VIEWSTATE and EVENTVALIDATION are dynamic
+    # ("btnCSV" should always = "CSV")
+    #
+    # Note: This process _should_ also work for house committees with some tweaking
+
+    doc = lxml.html.fromstring(response.text, response.url)
+    doc = lxml.html.make_links_absolute(doc)
+    # there is only one form on the page
+    form_response = lxml.html.submit_form(doc.forms[0], extra_values={"btnCSV": "CSV"})
+    csv_doc = lxml.html.parse(form_response).getroot()
+    return csv_doc
+
+
+class HouseList(CsvListPage):
+    # note: there is a CSV, that requires a form submission to obtain
     source = URL("https://house.mo.gov/MemberGridCluster.aspx?year=2021&code=R+&filter=clear")
-    selector = CSS("tr")
+    _member_base_url = "https://house.mo.gov/MemberDetails.aspx?year=2021&code=R&district={}"
 
-    def process_item(self, item):
-        tds = CSS("td").match(item, min_items=0, max_items=8)
-        if not tds:
+    def postprocess_response(self):
+        csv_doc = extract_csv(self.response)
+        self.reader = csv.DictReader(io.StringIO(csv_doc.text_content()))
+
+    def process_item(self, row):
+        if not row:
             self.skip()
-        _, last, first, district, party, town, phone, room = tds
-        if last.text_content() == "Vacant":
+        if row["LastName"] == "Vacant":
             self.skip()
+
         return HouseDetail(
             HousePartial(
-                last_name=last.text_content(),
-                first_name=first.text_content(),
-                district=int(district.text_content()),
-                party=party.text_content(),
-                hometown=town.text_content().strip(),
-                voice=phone.text_content(),
-                room=room.text_content(),
-                url=CSS("a").match_one(last).get("href"),
+                last_name=row["LastName"],
+                first_name=row["FirstName"],
+                district=int(row["District"]),
+                party=row["Party"],
+                hometown=row["HomeTown"].strip(),
+                voice=row["Phone"],
+                room=row["Room"],
+                url=self._member_base_url.format(row["District"]),
             )
         )
 
@@ -45,9 +75,10 @@ class HouseDetail(HtmlPage):
     input_type = HousePartial
 
     def process_page(self):
-        party = {"D": "Democratic", "R": "Republican"}[self.input.party]
+        party = {"Democrat": "Democratic", "Republican": "Republican"}[self.input.party]
 
         photo = CSS("img#ContentPlaceHolder1_imgPhoto1").match_one(self.root).get("src")
+        email = CSS('a[href^="mailto:"]').match_one(self.root).get("href").replace("mailto:", "")
 
         p = Person(
             state="mo",
@@ -58,6 +89,7 @@ class HouseDetail(HtmlPage):
             name=f"{self.input.first_name} {self.input.last_name}",
             given_name=self.input.first_name,
             family_name=self.input.last_name,
+            email=email,
         )
         # TODO
         # p.extras["hometown"] = self.input.hometown
